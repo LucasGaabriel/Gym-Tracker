756cd7a3374938ad3bff7fa206cece73ec507612
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2025-02-25 20:41:14 -0300

    Adiciona configurações iniciais para uso do Firebase e inicio da Activity de Login

---
+++ app/src/main/java/com/lucascosta/gymtracker/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.lucascosta.gymtracker
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val isLoggedIn = false//checkUserLoggedIn() // Sua lógica para verificar login
+
+        if (isLoggedIn) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
