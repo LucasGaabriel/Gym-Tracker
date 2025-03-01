2d5afc51f382e6e7a03cfd9859b2cbfc957d4f13
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2025-03-01 16:04:12 -0300

    Definido layout do Exercise Detail

--- app/src/main/java/com/lucascosta/gymtracker/ui/exercises/ExercisesFragment.kt
+++ app/src/main/java/com/lucascosta/gymtracker/ui/exercises/ExercisesFragment.kt
@@ -1,5 +1,7 @@
 package com.lucascosta.gymtracker.ui.exercises
 
+import com.lucascosta.gymtracker.R
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.lucascosta.gymtracker.databinding.FragmentExercisesBinding
 
-class ExercisesFragment : Fragment() {
+class ExercisesFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentExercisesBinding? = null
 
@@ -30,11 +32,20 @@ class ExercisesFragment : Fragment() {
         exercisesViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        binding.button.setOnClickListener(this)
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button) {
+            startActivity(Intent(context, ExerciseDetailActivity::class.java))
+        }
     }
 }
