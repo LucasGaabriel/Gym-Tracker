d01d19bf7577e8eab7342da08868806cb6993e88
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2025-03-04 19:06:20 -0300

    Configurada toolbar

--- app/src/main/java/com/lucascosta/gymtracker/MainActivity.kt
+++ app/src/main/java/com/lucascosta/gymtracker/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.lucascosta.gymtracker
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,19 +25,31 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_routines,
-                R.id.navigation_exercises,
-                R.id.navigation_profile
-
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home,
+//                R.id.navigation_routines,
+//                R.id.navigation_exercises,
+//                R.id.navigation_profile
+//
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        setToolbar()
+    }
+
+    fun setToolbar() {
+        val navHostFrag = supportFragmentManager.
+        findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFrag.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
