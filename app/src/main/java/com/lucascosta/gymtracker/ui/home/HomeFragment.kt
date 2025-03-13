5601dcaea6525b3f91730e2e96c920ec563d21a2
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2025-03-13 10:41:44 -0300

    Ajusta botões do fragment Home

--- app/src/main/java/com/lucascosta/gymtracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lucascosta/gymtracker/ui/home/HomeFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.lucascosta.gymtracker.databinding.FragmentHomeBinding
+import com.lucascosta.gymtracker.R
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -30,7 +32,21 @@ class HomeFragment : Fragment() {
 //        homeViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
+        binding.newExercise.setOnClickListener(this)
+        binding.personalInfo.setOnClickListener(this)
+
         return root
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.new_exercise) {
+            // Navegar para a tela de adicionar exercício
+            findNavController().navigate(R.id.action_navigation_home_to_navigation_add_exercise)
+        } else if (v.id == R.id.personal_info) {
+            // Navegar para a tela de perfil
+            findNavController().navigate(R.id.action_navigation_home_to_navigation_profile)
+        }
     }
 
     override fun onDestroyView() {
